8c27eae89832b5bd56036caa1d0107b5095c3690
Maruko <marco.k95@hotmail.com>
2020-01-22 17:11:26 -0300

    accessing fragments from bottom menu

--- app/src/main/java/com/mnakasima/helpiechallenge/view/MainActivity.kt
+++ app/src/main/java/com/mnakasima/helpiechallenge/view/MainActivity.kt
@@ -2,12 +2,42 @@ package com.mnakasima.helpiechallenge.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mnakasima.helpiechallenge.R
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId){
+            R.id.menu_users -> {
+                replaceFragment(UserFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.menu_photos -> {
+                replaceFragment(PhotoFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
