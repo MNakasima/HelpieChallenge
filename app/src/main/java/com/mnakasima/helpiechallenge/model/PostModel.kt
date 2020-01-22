4e8de9529bab2c3cf4b53adc250944e085a8bea1
Maruko <marco.k95@hotmail.com>
2020-01-22 18:26:18 -0300

    user and post model

---
+++ app/src/main/java/com/mnakasima/helpiechallenge/model/PostModel.kt
@@ -0,0 +1,7 @@
+package com.mnakasima.helpiechallenge.model
+
+data class PostModel(
+    val postId:String?,
+    val postTitle:String?,
+    val postText:String?
+)
---
+++ app/src/main/java/com/mnakasima/helpiechallenge/model/UserModel.kt
@@ -0,0 +1,9 @@
+package com.mnakasima.helpiechallenge.model
+
+data class User(
+    val userId:String?,
+    val userName:String?,
+    val userEmail:String?,
+    val userCompany:String?,
+    val userCity:String?
+)
